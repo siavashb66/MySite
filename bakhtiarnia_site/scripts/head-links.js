b16a6c04ea06740b341a982c4f175c14e320628c
Siavash <siavashb66@gmail.com>
2024-04-04 02:02:20 +0330

    updated headlinks, font awesome icons, updated thebe kernel

--- bakhtiarnia_site/scripts/head-links.js
+++ bakhtiarnia_site/scripts/head-links.js
@@ -1,5 +1,5 @@
 // Function to inject a link tag into the head
-function injectLinkTag(href, rel, type = "", color = "") {
+function injectLinkTag(href, rel, type = "", color = "", sizes = "") {
   const linkTag = document.createElement("link");
   linkTag.href = href;
   linkTag.rel = rel;
@@ -9,20 +9,23 @@ function injectLinkTag(href, rel, type = "", color = "") {
   if (color) {
     linkTag.setAttribute("color", color);  // Set the color attribute
   }
+  if (sizes) {
+    linkTag.setAttribute("sizes", sizes);  // Set the sizes attribute
+  }
   document.head.appendChild(linkTag);
 }
 
 // Inject all the favicon and related link tags
-injectLinkTag("/assets/favicons/apple-touch-icon-60x60.png", "apple-touch-icon", "image/png", "sizes=\"60x60\"");
-injectLinkTag("/assets/favicons/apple-touch-icon-76x76.png", "apple-touch-icon", "image/png", "sizes=\"76x76\"");
-injectLinkTag("/assets/favicons/apple-touch-icon-120x120.png", "apple-touch-icon", "image/png", "sizes=\"120x120\"");
-injectLinkTag("/assets/favicons/apple-touch-icon-152x152.png", "apple-touch-icon", "image/png", "sizes=\"152x152\"");
-injectLinkTag("/assets/favicons/apple-touch-icon-180x180.png", "apple-touch-icon", "image/png", "sizes=\"180x180\"");
-injectLinkTag("/assets/favicons/favicon-32x32.png", "icon", "image/png", "sizes=\"32x32\"");
-injectLinkTag("/assets/favicons/favicon-16x16.png", "icon", "image/png", "sizes=\"16x16\"");
+injectLinkTag("/assets/favicons/apple-touch-icon-60x60.png", "apple-touch-icon", "", "", "60x60");
+injectLinkTag("/assets/favicons/apple-touch-icon-76x76.png", "apple-touch-icon", "", "", "76x76");
+injectLinkTag("/assets/favicons/apple-touch-icon-120x120.png", "apple-touch-icon", "", "", "120x120");
+injectLinkTag("/assets/favicons/apple-touch-icon-152x152.png", "apple-touch-icon", "", "", "152x152");
+injectLinkTag("/assets/favicons/apple-touch-icon-180x180.png", "apple-touch-icon", "", "", "180x180");
+injectLinkTag("/assets/favicons/favicon-32x32.png", "icon", "image/png", "", "32x32");
+injectLinkTag("/assets/favicons/favicon-16x16.png", "icon", "image/png", "", "16x16");
 injectLinkTag("/assets/favicons/site.webmanifest", "manifest");
-injectLinkTag("/assets/favicons/safari-pinned-tab.svg", "mask-icon", "image/svg+xml", "#5bbad5");  // Include color attribute
+injectLinkTag("/assets/favicons/safari-pinned-tab.svg", "mask-icon", "image/svg+xml", "#5bbad5");
 injectLinkTag("/favicon.ico", "shortcut icon");
 injectLinkTag("", "msapplication-TileColor", "", "#da532c");
-injectLinkTag("/assets/favicons/browserconfig.xml", "msapplication-config", "");
+injectLinkTag("/assets/favicons/browserconfig.xml", "msapplication-config");
 injectLinkTag("", "theme-color", "", "#ffffff");
