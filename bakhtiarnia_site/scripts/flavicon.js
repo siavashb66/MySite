fa47daed1f0a553fc87d8f031e58be9d6a54d96a
Siavash <siavashb66@gmail.com>
2024-04-05 22:59:37 +0330

    updated flavicon, display span font to none for max-width 480px, added flavicon.js script

---
+++ bakhtiarnia_site/scripts/flavicon.js
@@ -0,0 +1,33 @@
+function appendMetaTag(name, content) {
+  const headElement = document.head;
+  const metaElement = document.createElement('meta');
+  metaElement.name = name;
+  metaElement.content = content;
+  headElement.appendChild(metaElement);
+}
+
+// Usage for meta tags
+document.addEventListener('DOMContentLoaded', (event) => {
+  appendMetaTag('msapplication-TileColor', '#dc6946');
+  appendMetaTag('theme-color', '#ffffff');
+});
+
+function appendLinkElement(rel, sizes, type, href) {
+  var linkElement = document.createElement('link');
+  
+  if (rel) linkElement.rel = rel;
+  if (sizes) linkElement.sizes = sizes;
+  if (type) linkElement.type = type;
+  if (href) linkElement.href = href;
+
+  document.head.appendChild(linkElement);
+}
+
+// Usage for link elements
+document.addEventListener('DOMContentLoaded', function() {
+  appendLinkElement('apple-touch-icon', '180x180', null, '/apple-touch-icon.png');
+  appendLinkElement('icon', '32x32', 'image/png', '/favicon-32x32.png');
+  appendLinkElement('icon', '16x16', 'image/png', '/favicon-16x16.png');
+  appendLinkElement('manifest', null, null, '/site.webmanifest');
+  appendLinkElement('mask-icon', null, null, '/safari-pinned-tab.svg');
+});
