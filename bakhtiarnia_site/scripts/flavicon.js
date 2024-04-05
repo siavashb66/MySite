86506f3fc5cdb3df4ab4a9ec6e6e7ef7525d741a
Siavash <siavashb66@gmail.com>
2024-04-06 02:15:00 +0330

    updated sitemanifest, updated flavicons.js

--- bakhtiarnia_site/scripts/flavicon.js
+++ bakhtiarnia_site/scripts/flavicon.js
@@ -25,9 +25,12 @@ function appendLinkElement(rel, sizes, type, href) {
 
 // Usage for link elements
 document.addEventListener('DOMContentLoaded', function() {
-  appendLinkElement('apple-touch-icon', '180x180', null, './apple-touch-icon.png');
-  appendLinkElement('icon', '32x32', 'image/png', './favicon-32x32.png');
-  appendLinkElement('icon', '16x16', 'image/png', './favicon-16x16.png');
-  appendLinkElement('manifest', null, null, './site.webmanifest');
-  appendLinkElement('mask-icon', null, null, './safari-pinned-tab.svg');
+  appendLinkElement('apple-touch-icon', '180x180', 'image/png', '/apple-touch-icon.png');
+  appendLinkElement('icon', '32x32', 'image/png', '/favicon-32x32.png');
+  appendLinkElement('icon', '16x16', 'image/png', '/favicon-16x16.png');
+  appendLinkElement('manifest', null, null, '/site.webmanifest');
+  appendLinkElement('mask-icon', null, null, '/safari-pinned-tab.svg');
+  appendLinkElement('icon', null, 'image/x-icon', '/favicon.ico');
+  appendLinkElement('icon', '512x512', 'image/png', '/android-chrome-512x512.png');
+  appendLinkElement('icon', '192x192', 'image/png', '/android-chrome-192x192.png');
 });
