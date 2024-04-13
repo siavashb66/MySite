6e4602120459e73cd1bcfd4ee5a5157a50e84061
Siavash <siavashb66@gmail.com>
2024-04-13 15:34:41 +0330

    extended dynamic quotes to fr/fa/en for explorations page

--- bakhtiarnia_site/scripts/quoteFetcher.js
+++ bakhtiarnia_site/scripts/quoteFetcher.js
@@ -1,7 +1,14 @@
-const fallbackQuote = '"Not all those who wander are lost." - J.R.R. Tolkien';
+const fallbackQuotes = {
+    'en': '"Not all those who wander are lost." – J.R.R. Tolkien',
+    'fa': '«آن‌هایی که سرگردانند، همه گم نشده‌اند.» - جی. آر. آر. تالکین',
+    'fr': '"Ce ne sont pas tous ceux qui errent qui sont perdus." – J.R.R. Tolkien'
+};
 
 function fetchQuote() {
-    Papa.parse('/assets/data/quotes.csv', {
+    let lang = document.documentElement.lang; // Get language from HTML tag
+    let fallbackQuote = fallbackQuotes[lang] || fallbackQuotes['en']; // Get fallback quote for the language
+
+    Papa.parse(`/assets/data/quotes-${lang}.csv`, {
         download: true,
         header: true,
         complete: function(results) {
