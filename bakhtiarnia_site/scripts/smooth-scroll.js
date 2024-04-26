ab10f234056efafed6b728635d5d476ba74db81d
Siavash <siavashb66@gmail.com>
2024-04-26 05:13:50 +0330

    smooth scroll (offset correction), html favicon tags, scroll to top styling modification, mobile landscape styling, hello-world fix, headings size mobile

---
+++ bakhtiarnia_site/scripts/smooth-scroll.js
@@ -0,0 +1,23 @@
+// Smooth Scroll | offset adjustment of link position
+const LINKOFFSET = 120; // Offset for scrolling
+  
+$('#navbar-content li a').click(function(event) {
+  const targetElement = $($(this).attr('href'));
+  
+  if(targetElement.length) {
+    const targetPosition = targetElement[0].offsetTop - LINKOFFSET;
+  
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
+
+    // Update the URL without causing a page refresh
+    history.pushState(null, null, $(this).attr('href'));
+  } else {
+    console.warn('Target element does not exist');
+  }
+
+  // Prevent the default action of the event
+  event.preventDefault();
+});
